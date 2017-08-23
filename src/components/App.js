5b7dc1add118968eec87c39cef54082c9b885e83
Steven Difelippo <matrixdriver04@gmail.com>
2017-08-23 18:33:57 -0400

    almost there

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
 import './styles/App.css';
+import EarthquakeInfo from './EarthquakeInfo.js'
+import EarthquakeList from './EarthquakeList.js'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="title">
-          <div className="Earthquakes!">
-            <p className="App-intro">
-              To get started, edit <code>src/App.js</code> and save to reload.
-            </p>
-      </div>
+          <div className="my-header!">
+          Earthquakes!!!
+
+      <earthquakeInfo/>
+      <earthquakeList/>
+      <div/>
+      <div/>
     );
   }
 }
